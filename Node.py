dc6e562098be61ea8d484f98b24e4d87bac72a34
akonneker <adam.m.konneker@gmail.com>
2011-01-21 00:18:44 -0700

    base nodes now work

--- Node.py
+++ Node.py
@@ -1,18 +1,17 @@
-from numpy import Array
-
+from numpy import array
 class Node:
-    """A node representing one element of a model carbon nanotube"""
-	def __init__(self):
+	"""A node representing one element of a model carbon nanotube"""
+	def __init__(self, pos):
 		self.prev = [None, None, None]
 		self.next = [None, None, None]
-		self.loc = array([0 0 0])
-		self.vel = array([0 0 0])
-		self.force = array([0 0 0])
-		self.midLoc = array([0 0 0])
-		self.midVel = array([0 0 0])
-		self.midForce = array([0 0 0])
+		self.loc = array([0, 0, 0])
+		self.vel = array([0, 0, 0])
+		self.force = array([0, 0, 0])
+		self.midLoc = array([0, 0, 0])
+		self.midVel = array([0, 0, 0])
+		self.midForce = array([0, 0, 0])
 		self.mass = 0
-		self.fixed = false
+		self.fixed = False
 		
 
 
--- cntsim.py
+++ cntsim.py
@@ -1,9 +1,11 @@
-from Bin import Bin
 '''
 Created on Jan 17, 2010
 
 @author: sahar
 '''
+from Bin import Bin
+from Node import Node
+import math
 import numpy as np
 import xml.etree.ElementTree as ET
 
@@ -21,11 +23,29 @@ nGrid = int(np.floor(bBoxLength/(maxRad*3))) #number of cells per side of grid
 sGrid = float(bBoxWidth)/nGrid #size of grid cells
 
 binList = np.empty([nGrid,nGrid,nGrid],dtype = np.object_)
-
+index = 0
 for bx in range(nGrid):
 	for by in range(nGrid):
 		for bz in range(nGrid):
 			binList[bx][by][bz] = Bin(position = np.array([bx*sGrid, by*sGrid, bz*sGrid]), dims = np.array([sGrid, sGrid, sGrid]))
+			
+nodeNum = int(np.ceil(bBoxArea*pDensity/(np.pi*meanRad**2)))
+nodeMap = {}
+for i in range(nodeNum):
+	added = False
+	while not added:
+		tx = np.random.randint(0,nGrid)
+		ty = np.random.randint(0,nGrid)
+		tz = 0
+		if(binList[tx][ty][tz].nodeList == []):
+			binList[tx][ty][tz].nodeList.append(Node(binList[tx][ty][tz].dimList + [.5*sGrid,.5*sGrid,meanRad]))
+			nodeMap[(tx,ty)] = binList[tx][ty][tz].nodeList[0]
+			added = True
+
+print nodeMap.keys()
+
+
+			
 			
 
 
