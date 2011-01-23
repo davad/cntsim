c680d4e159d4d4a82a66d62bd671a5e47b020852
David Landry <david@dmwl.net>
2011-01-22 19:02:07 -0700

    Real config file and defaults

--- Config.py
+++ Config.py
@@ -11,6 +11,7 @@ Set actual default settings
 '''
 
 import json
+import math
 
 def write(dictionary, filename='config.json'):
   fp = open(filename,'w')
@@ -20,7 +21,53 @@ def write(dictionary, filename='config.json'):
 def read(filename='config.json'):
   # Declare default settings
   settings = {
-    "key" : "value",
+    "BIN" : {
+      "MAX_X" : 100,
+      "MAX_Y" : 100,
+      "MAX_Z" : 100,
+    },
+    "FOREST" : {
+      "DIMENSIONS" : {
+        "MAX_X" : 1000,
+        "MAX_Y" : 1000,
+        "MAX_Z" : 1000,
+      },
+      "SURFACE_DENSITY" : 0.02,
+    },
+    "NODE" : {
+      "NODE_MASS" : 1.0,
+      "MEAN_RADIUS" : 4.0,
+      "MIN_RADIUS" : 2.0,
+      "MAX_RADIUS" : 6.0,
+      "SIGMA_RADIUS" : .45,
+    },
+    "TUBE": {
+      "SEGMENT_LENGTH" : 8.0,
+      "SEGMENT_NUM" : 50,
+    },
+    "SIMULATION" : {
+      "TIME_STEP" : .01,
+      "MIN_DIST" : .001,
+      "ATTRACT_RADIUS" : 3,
+      "GRAVITY" : .1,
+      "GROWTH_SPEED" : 10.2,
+    },
+    "SPRINGS" : {
+      "ANGLES" : {
+        "MAX_THETA" : 2*math.pi,
+        "MAX_PHI" : math.pi/2,
+        "MEAN_PHI" : math.pi/4.0,
+        "SIGMA_PHI" : 0.282387/2,
+      },
+      "CONSTANTS" : {
+        "EDGE" : 100,
+        "TORSION" : 100,
+      },
+      "DAMPING" : {
+        "TORSION" : math.sqrt(100),
+        "LINEAR" : math.sqrt(100),
+      }
+    }, 
   }
 
   # Try to load the config file and merge into settings
