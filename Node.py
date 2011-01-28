4d16c2f6605e415abacb58b0833fd9563fcc1d2b
akonneker <adam.m.konneker@gmail.com>
2011-01-27 23:17:36 -0700

    removed cruft

--- Node.py
+++ Node.py
@@ -1,17 +1,17 @@
 from numpy import array
 class Node:
 	"""A node representing one element of a model carbon nanotube"""
-	def __init__(self, pos):
+	def __init__(self, pos = array([0, 0, 0]), mass = 1, fixed = False):
 		self.prev = [None, None, None]
 		self.next = [None, None, None]
-		self.loc = array([0, 0, 0])
+		self.pos = pos
 		self.vel = array([0, 0, 0])
 		self.force = array([0, 0, 0])
-		self.midLoc = array([0, 0, 0])
+		self.midPos = array([0, 0, 0])
 		self.midVel = array([0, 0, 0])
 		self.midForce = array([0, 0, 0])
-		self.mass = 0
-		self.fixed = False
+		self.mass = mass
+		self.fixed = fixed
 		
 
 
--- cntsim.py
+++ cntsim.py
@@ -18,19 +18,51 @@ bBoxLength = 300
 pDensity = .05 #Unitless fraction covered by particles
 bBoxArea = bBoxLength*bBoxWidth
 areaC = 0
+ANGULAR_MEAN = np.pi/4.0
+ANGULAR_SIGMA = 0.282387/2
+THETA_MIN = 0
+THETA_MAX = 2*np.pi
+NODE_MASS = 10
 
 nGrid = int(np.floor(bBoxLength/(maxRad*3))) #number of cells per side of grid
 sGrid = float(bBoxWidth)/nGrid #size of grid cells
 
 binList = np.empty([nGrid,nGrid,nGrid],dtype = np.object_)
-index = 0
 for bx in range(nGrid):
 	for by in range(nGrid):
 		for bz in range(nGrid):
 			binList[bx][by][bz] = Bin(position = np.array([bx*sGrid, by*sGrid, bz*sGrid]), dims = np.array([sGrid, sGrid, sGrid]))
+
+for bx in range(nGrid):
+	for by in range(nGrid):
+		for bz in range(nGrid):
+			if bx is 0:
+				xp = nGrid-1
+			else: xp = bx-1
+			if by is 0:
+				yp = nGrid-1
+			else: yp = by-1
+			if bz is 0:
+				zp = nGrid-1
+			else: zp = bz-1
 			
+			if bx is nGrid-1:
+				xn = 0
+			else: xn = bx+1
+			if by is nGrid-1:
+				yn = 0
+			else: yn = by+1
+			if bz is nGrid-1:
+				zn = 0
+			else: zn = bz+1
+			
+			binList[bx][by][bz].neighborList.extend([binList[bx][by][zn], binList[bx][by][zp], binList[bx][yn][bz], binList[bx][yp][bz], binList[xn][by][bz], binList[xp][by][bz]])
+			binList[bx][by][bz].neighborList.extend([binList[bx][yn][zn], binList[bx][yn][zp], binList[bx][yp][zn], binList[bx][yp][zp], binList[xn][by][zn], binList[xp][by][zn], binList[xn][by][zp], binList[xp][by][zp], binList[xn][yn][bz], binList[xn][yp][bz], binList[xp][yn][bz], binList[xp][yp][bz]])
+			binList[bx][by][bz].neighborList.extend([binList[xn][yn][zn], binList[xn][yn][zp], binList[xn][yp][zn], binList[xn][yp][zp], binList[xp][yn][zn], binList[xp][yn][zp], binList[xp][yp][zn], binList[xp][yp][zp]])
+
+
 nodeNum = int(np.ceil(bBoxArea*pDensity/(np.pi*meanRad**2)))
-nodeMap = {}
+baseList = []
 for i in range(nodeNum):
 	added = False
 	while not added:
@@ -38,11 +70,18 @@ for i in range(nodeNum):
 		ty = np.random.randint(0,nGrid)
 		tz = 0
 		if(binList[tx][ty][tz].nodeList == []):
-			binList[tx][ty][tz].nodeList.append(Node(binList[tx][ty][tz].dimList + [.5*sGrid,.5*sGrid,meanRad]))
-			nodeMap[(tx,ty)] = binList[tx][ty][tz].nodeList[0]
+			binList[tx][ty][tz].nodeList.append(Node(binList[tx][ty][tz].dimList + [.5*sGrid,.5*sGrid,meanRad], NODE_MASS, True))
+			baseList.append(binList[tx][ty][tz].nodeList[0])
 			added = True
 
-print nodeMap.keys()
+for baseNode in baseList:
+	phi = np.random.normal(ANGULAR_MEAN, ANGULAR_SIGMA)
+	theta = np.random.rand()*THETA_MAX
+	newNode = Node(baseNode.pos + .001 * np.array([np.sin(phi)*np.cos(theta), np.sin(phi)*np.sin(theta), np.cos(phi)]), NODE_MASS)
+	baseNode.next[0] = newNode
+	newNode.prev[0] = baseNode
+	
+
 
 
 			
