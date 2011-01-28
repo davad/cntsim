4c8ec7b58575893ae80c9695113145865432989f
akonneker <adam.m.konneker@gmail.com>
2011-01-27 22:44:11 -0700

    put old code in its own folder to reduce cruftiness

---
+++ oldcode/XMLParser.py
@@ -0,0 +1,53 @@
+'''
+Created on Jul 15, 2010
+
+@author: sahar
+'''
+
+#The ElementTree library is a builtin library for XML manipulation
+import xml.etree.ElementTree as ET
+import numpy as np
+
+#Reads the xml contained in the specified file or thows an exception
+FILE_NAME = "test1.xml"
+POSITION_LENGTH = 3
+X = 0
+Y = 1
+Z = 2
+
+#Most of the magic happens here, and it's done automagically
+xml = ET.parse(FILE_NAME)
+
+#Set the root of the document, which contains attributes that describe 
+#the properties of the forest, such as # of nodes.
+#It is currently assumed that all tubes have the same number of nodes
+CNTForest = xml.getroot()
+
+#
+nodeNum = int(CNTForest.attrib["nodenum"])
+tubeNum = int(CNTForest.attrib["tubenum"])
+minRadius = float(CNTForest.attrib["rmin"])
+maxRadius = float(CNTForest.attrib["rmax"])
+dimensions = [float(CNTForest.attrib["xmax"]),float(CNTForest.attrib["ymax"]),float(CNTForest.attrib["zmax"])]
+print("Number of nodes in file: " + str(nodeNum))
+print("Number of tubes in file: " + str(tubeNum))
+print("Minimum radius of any tube in forest: " + str(minRadius))
+print("Maximum radius of any tube in forest: " + str(maxRadius))
+print("Dimensions of forest: " + str(dimensions))
+
+#The data is separated into two arrays: radii is a (1, T) length array
+#where T is the number of tubes
+radii = np.zeros((tubeNum))
+#positions contains the position data in the same order as radii
+#it's shape is (T,N,3) where N is the number of nodes per tube, and T defined prior
+positions = np.zeros((tubeNum, nodeNum/tubeNum, POSITION_LENGTH))
+
+#Iterates over the XML tree and puts stuff where it's supposed to go in the separate arrays
+indT = 0
+for tube in CNTForest.getiterator("T"):
+    radii[indT] = float(tube.attrib["r"])
+    indN = 0
+    for node in tube.getiterator("N"):
+        positions[indT,indN] = np.array([float(node.attrib["x"]),float(node.attrib["y"]),float(node.attrib["z"])])
+        indN += 1
+    indT += 1
