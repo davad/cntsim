23b24dd7bbf508f3cd8716a999587c64beeb13fd
unknown <zinanamk@.physics.byu.edu>
2011-03-23 18:39:01 -0600

    collision aviodance and random placement work. toiroidal boundary conditions do not.

--- GridFactory.py
+++ GridFactory.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy import random
 import xml.etree.ElementTree as ET
+from Bin import Bin
 
 cellLength = 100
 tubeRadius = 4
@@ -15,8 +16,6 @@ tubeDy = tubeDx*np.sqrt(3)/2.0
 tubeNx = int(np.floor(cellLength)/np.ceil(tubeDx))
 tubeNy = int(np.floor(cellLength)/np.ceil(tubeDy))
 
-nGrid = int(np.floor(cellLength/(tubeRadius*3))) #number of cells per side of grid
-sGrid = float(cellLength)/nGrid #size of grid cells
 xInd = 0
 yInd = 1
 zInd  = 2
@@ -24,189 +23,69 @@ rInd = 3
 nIter = 500;
 
 
+nGrid = int(np.floor(cellLength/(tubeRadius*3))) #number of cells per side of grid
+sGrid = float(cellLength)/nGrid #size of grid cells
+binList = np.empty([nGrid,nGrid],dtype = np.object_)
+
+class baseNode:
+	def __init__(self, pos = np.array([0,0,0]), edgePos = np.array([0,0,0]), radius = 0.0, edge = False):
+		self.pos = pos
+		self.edgePos = edgePos
+		self.radius = radius
+		self.edge = edge
+		
+
+def overCheck(testV):
+	tI = int(np.floor(testV[xInd]/sGrid))
+	tJ = int(np.floor(testV[yInd]/sGrid))
+	if tI < 0:
+		tI = 0
+	elif tI > nGrid-1:
+		tI = nGrid-1
+	if tJ < 0:
+		tJ = 0
+	elif tJ > nGrid-1:
+		tJ = nGrid-1
+	for node in tubeList:
+		if np.inner( testV[0:3]-node[0:3],testV[0:3]-node[0:3] ) < 4*(tubeRadius)**2:
+			return node
+	# for node in binList[tI][tJ].nodeList:
+		# if np.inner(testV[0:3]-node[0:3],testV[0:3]-node[0:3] ) < 4*(tubeRadius)**2:
+			# return node
+	# for nbr in binList[tI][tJ].neighborList:
+		# for node in nbr.nodeList:
+			# if np.inner(testV[0:3]-node[0:3],testV[0:3]-node[0:3] ) < 4*(tubeRadius)**2:
+				# return node
+	#return None
+
+
+for bx in range(nGrid):
+	for by in range(nGrid):
+		binList[bx][by] = Bin(position = np.array([bx*sGrid, by*sGrid]), dims = np.array([sGrid, sGrid]))
+
+for bx in range(nGrid):
+	for by in range(nGrid):
+		if bx is 0:
+			xp = nGrid-1
+		else: xp = bx-1
+		if by is 0:
+			yp = nGrid-1
+		else: yp = by-1
+		
+		if bx is nGrid-1:
+			xn = 0
+		else: xn = bx+1
+		if by is nGrid-1:
+			yn = 0
+		else: yn = by+1
+		#Clockwise order starting from top
+		binList[bx][by].neighborList.extend( [binList[bx][yn],binList[xn][yn],binList[xn][by],binList[xn][yp],binList[bx][yp],binList[xp][yp],binList[xp][by],binList[xp][yn]])
+
+
 field = [None]*nGrid
 
 for i in range(nGrid):
     field[i] = [None]*nGrid
-
-
-def overCheckF(testV):
-    #Checks for overlaps among nodes placed with the grid cell of the new node
-    # and also the 8 cells surrounding it
-    
-    global field, sGrid, xInd, yInd
-    fIT = int(np.floor(testV[xInd]/sGrid))
-    fJT = int(np.floor(testV[yInd]/sGrid))
-    
-    #test tubes in same block, if any
-    outToop = overCheckG(testV, (fIT, fJT))
-    if outToop[0]:
-        return outToop[1]
-#Cases are processed as follows:
-##O######xmax        
-###|6|7|8|###       
-###|5|0|1|###
-###|4|3|2|###
-#ymax########     
-    if (fIT > 0 and fIT < nGrid-1) and (fJT > 0 and fJT < nGrid-1): #xmid & ymid
-        outToop = overCheckG(testV, (fIT + 1, fJT + 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT - 1, fJT - 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT + 1, fJT - 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT - 1, fJT + 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT, fJT + 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT, fJT - 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT + 1, fJT))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT - 1, fJT))
-        if outToop[0]:
-            return outToop[1]
-    elif (fIT == nGrid-1) and (fJT > 0 and fJT < nGrid-1): #xmax & ymid
-        outToop = overCheckG(testV, (fIT - 1, fJT - 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT - 1, fJT + 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT, fJT + 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT, fJT - 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT - 1, fJT))
-        if outToop[0]:
-            return outToop[1]
-    elif (fIT == nGrid-1) and (fJT == nGrid-1): #xmax & ymax
-        outToop = overCheckG(testV, (fIT - 1, fJT - 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT, fJT - 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT - 1, fJT))
-        if outToop[0]:
-            return outToop[1]
-    elif (fIT > 0 and fIT < nGrid-1) and (fJT == nGrid-1): #xmid & ymax
-        outToop = overCheckG(testV, (fIT - 1, fJT - 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT + 1, fJT - 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT, fJT - 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT + 1, fJT))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT - 1, fJT))
-        if outToop[0]:
-            return outToop[1]
-    elif (fIT == 0) and (fJT == nGrid-1): #xmin & ymax
-        outToop = overCheckG(testV, (fIT + 1, fJT - 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT, fJT - 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT + 1, fJT))
-        if outToop[0]:
-            return outToop[1]
-    elif (fIT == 0) and (fJT > 0 and fJT < nGrid-1): #xmin & ymid
-        outToop = overCheckG(testV, (fIT + 1, fJT + 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT + 1, fJT - 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT, fJT + 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT, fJT - 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT + 1, fJT))
-        if outToop[0]:
-            return outToop[1]
-    elif (fIT == 0) and (fJT == 0): #xmin & ymin
-        outToop = overCheckG(testV, (fIT + 1, fJT + 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT, fJT + 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT + 1, fJT))
-        if outToop[0]:
-            return outToop[1]
-    elif (fIT > 0 and fIT < nGrid-1) and (fJT == 0): #xmid & ymin
-        outToop = overCheckG(testV, (fIT + 1, fJT + 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT - 1, fJT + 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT, fJT + 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT + 1, fJT))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT - 1, fJT))
-        if outToop[0]:
-            return outToop[1]
-    elif (fIT == nGrid-1) and (fJT == 0): #xmax & ymin
-        outToop = overCheckG(testV, (fIT - 1, fJT + 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT, fJT + 1))
-        if outToop[0]:
-            return outToop[1]
-        outToop = overCheckG(testV, (fIT - 1, fJT))
-        if outToop[0]:
-            return outToop[1]
-
-    return None
-    
-def overCheckG(testV, indices):
-    #tests is the position vector will overlap with a previously existing position
-    # by default, this function returns 
-    global field, sGrid, xInd, yInd
-    fIT = indices[xInd]
-    fJT = indices[yInd]
-    outToop = (False, None)
-    #test tubes in block indicated by indices
-    if field[fIT][fJT]!= None:
-        if len(np.shape(field[fIT][fJT])) < 2:
-            outToop = overCheckV(testV, field[fIT][fJT])
-        else:
-            for v in field[fIT][fJT]:
-                outToop = overCheckV(testV, v)
-                if outToop[0]:
-                    break
-    
-    return outToop
-
-def overCheckV(testV1, testV2):
-    global xInd, yInd, rInd
-    if pow(testV1[xInd] - testV2[xInd],2)+pow(testV1[yInd] - testV2[yInd],2) < (testV1[rInd] + testV2[rInd])**2:
-        outToop = (True, testV2)
-    else: outToop = (False, None)
-    
-    return outToop	
-	
 	
 	
 nT = 0
@@ -215,7 +94,11 @@ for j in range(tubeNy):
 		if nT == tubeNum:
 			break
 		else:
-			tubeList.append(np.array([i*tubeDx+tubeRadius,j*tubeDy+tubeRadius, 0, tubeRadius]))
+			tTube = np.array([i*tubeDx+tubeRadius,j*tubeDy+tubeRadius, 0, tubeRadius])
+			tI = int(np.floor(tTube[xInd]/sGrid))
+			tJ = int(np.floor(tTube[yInd]/sGrid))
+			tubeList.append(tTube)
+			binList[tI][tJ].nodeList.append(tTube)
 			nT = nT + 1
 			
 # for j in range(tubeNy):
@@ -233,11 +116,32 @@ for i in range(nIter):
 	for v in tubeList:
 		tv = np.zeros(4)
 		tv[0:] = v[0:]
-		tv[0:3] = tv[0:3] + random.uniform(-tubeDy,tubeDy,[1,3])
+		tv[0:2] = tv[0:2] + random.uniform(-tubeDy,tubeDy,[1,2])
+		overV = overCheck(tv)
+		for n in range(2):
+			if tv[n] > cellLength:
+				tv[n] = tv[n] - cellLength
+			elif tv[n] < 0:
+				tv[n] = tv[n] + cellLength
 		#print tv
-		overV = overCheckF(tv)
 		if overV is None:
-			v[0:] = tv[0:]
+			overV = overCheck(tv)
+		if overV is None:
+			pI = int(np.floor(v[xInd]/sGrid))
+			pJ = int(np.floor(v[xInd]/sGrid))
+			nI = int(np.floor(tv[xInd]/sGrid))
+			nJ = int(np.floor(tv[xInd]/sGrid))
+			if pI != nI or pJ != nJ:
+				nList = binList[int(np.floor(v[xInd]/sGrid))][int(np.floor(v[yInd]/sGrid))].nodeList
+				for n in range(len(nList)) :
+					if (nList[n] == v).all():
+						nList.pop(n)
+						break
+				v[0:] = tv[0:]
+				binList[int(np.floor(v[xInd]/sGrid))][int(np.floor(v[yInd]/sGrid))].nodeList.append(v)
+			else:
+				v[0:] = tv[0:]
+			
 			
 CNT_Forest = ET.Element("CNTForest", xmax = str(cellLength), ymax = str(cellLength), zmax = str(0), tubenum = str(tubeNum), nodenum = str(tubeNum), rmax = str(tubeRadius), rmin = str(tubeRadius))
 for v in tubeList:
